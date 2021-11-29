a5ccdf0c055007c4d503168a1ab5783f7065d127
Josh Farish <josh_farish1@baylor.edu>
2021-11-29 15:15:42 -0600

    Added topics page

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 app_name = 'MainApp'
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('topics',views.topics,name='topics'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.all().order_by('date_added')
+
+    context = {'topics': topics}
+
+    return render(request, 'MainApp/topics.html', context)
