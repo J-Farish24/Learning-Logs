a8377d542c37f58ed620f0a9cfb37ef4013e3887
Josh Farish <josh_farish1@baylor.edu>
2021-12-01 15:44:50 -0600

    Created forms to add a new topic and a new entry to a topic

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('topics',views.topics,name='topics'),
     path('topics/<int:topic_id>/',views.topic,name='topic'),
+    path('new_topic/',views.new_topic,name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry,name='new_entry'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import TopicForm, EntryForm
 from .models import Topic
 
 # Create your views here.
@@ -19,3 +19,33 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
 
     return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+    
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect('MainApp:topic', topic_id=topic_id)
+    
+    context = {'form':form, 'topic': topic}
+    return render(request, 'MainApp/new_entry.html', context)
