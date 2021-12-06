20534abd0557141c2e34b93ffef787f65f4a107e
Josh Farish <josh_farish1@baylor.edu>
2021-12-06 15:34:58 -0600

    Added logout page and registration page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
+    #Registration page
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    #Register a new users
+    if request.method!= 'POST':
+        #Display blank registration form
+        form = UserCreationForm()
+    else:
+        #Process completed form
+        form = UserCreationForm(data=request.POST)
+    #Username has the appropriate characters, the passwords match,
+    #and the user isn't trying to do anything malicious in their submission
+        if form.is_valid():
+            new_user = form.save()
+            #Log the user in and redirect to homepage
+            login(request, new_user)
+            return redirect('MainApp:index')
+
+    #Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
