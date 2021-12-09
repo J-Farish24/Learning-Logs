b7401b3f09071bb369d4a6cbb79e883029cb7438
Josh Farish <josh_farish1@baylor.edu>
2021-12-09 15:38:13 -0600

    Changed 404 to occur when user requests an unkown topic

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .forms import TopicForm, EntryForm
 from .models import Topic, Entry
 from django.contrib.auth.decorators import login_required
@@ -19,7 +19,7 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     #Make sure the topic belongs to the current user
